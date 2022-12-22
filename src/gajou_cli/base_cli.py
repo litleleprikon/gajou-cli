be73bae374696f0d9156293980e943d4f234cf8d
emil.sharifullin <emil.sharifullin@sbermarket.ru>
2022-12-22 18:17:14 +0300

    feat: Add sudo support
    In some cases commands require to be ran under root. to satisfy this add
    additional option sudo that will prefix sudo to command

--- src/gajou_cli/base_cli.py
+++ src/gajou_cli/base_cli.py
@@ -7,11 +7,12 @@ from .return_codes import ReturnCodes
 
 
 class BaseCLI:
-    def __init__(self, name, custom_path, allure=None):
+    def __init__(self, name, custom_path, allure=None, sudo=False):
         self.name = name
         self.path = custom_path
         self.allure = allure
         self.logger = logging.getLogger(__name__)
+        self.sudo = sudo
 
     def _log_request(self, command, arguments):
         rq_msg = f'INPUT: {self.name} {command or ""}'
@@ -33,9 +34,16 @@ class BaseCLI:
         self._log_request(command, _args)
         start_time = time.time()
 
-        _cmd = [f'{self.path}{self.name}',
-                *command.split(' '),
-                *[arg for arg_group in _args for arg in arg_group.split(' ')]]
+        _cmd = []
+
+        if self.sudo:
+            _cmd.append("sudo")
+
+        _cmd.extend([
+            f'{self.path}{self.name}', *command.split(' '),
+            *[arg for arg_group in _args for arg in arg_group.split(' ')]
+        ])
+
         process = Popen(_cmd, stdout=PIPE, stderr=PIPE)
         output, error = (_bytes.decode().strip() for _bytes in process.communicate())
 
