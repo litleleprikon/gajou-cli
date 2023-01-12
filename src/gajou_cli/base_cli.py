c4a5c4628075ec0fd3916e8b2317d363d2ae22d1
Artyom Komarenko <artem.komarenko@sbermarket.ru>
2023-01-12 13:34:53 +0300

    Fix multi-words flag problem

--- src/gajou_cli/base_cli.py
+++ src/gajou_cli/base_cli.py
@@ -7,15 +7,15 @@ from .return_codes import ReturnCodes
 
 
 class BaseCLI:
-    def __init__(self, name, custom_path, allure=None, sudo=False):
+    def __init__(self, name, custom_path='', allure=None, sudo=False):
         self.name = name
         self.path = custom_path
         self.allure = allure
         self.logger = logging.getLogger(__name__)
         self.sudo = sudo
 
-    def _log_request(self, command, arguments):
-        rq_msg = f'INPUT: {self.name} {command or ""}'
+    def _log_request(self, command, arguments, sudo):
+        rq_msg = f'INPUT: {"sudo " if sudo else ""}{self.name} {command or ""}'
         if arguments:
             rq_msg += f' {" ".join(arguments)}'
         self.logger.info(rq_msg)
@@ -29,20 +29,16 @@ class BaseCLI:
         if self.allure:
             self.allure.attach(rs_msg, 'OUTPUT', attachment_type=self.allure.attachment_type.TEXT)
 
-    def do(self, command: str, *args) -> CLIResponse:
-        _args = list(filter(None.__ne__, args)) if args and args != (None,) else []
-        self._log_request(command, _args)
+    def do(self, command: str, *args, **kwargs) -> CLIResponse:
+        _args = [arg for arg_group in args for arg in _parse_command(arg_group)]
+
+        is_sudo = self.sudo or ('sudo' in kwargs and kwargs.get('sudo'))
+
+        _cmd = ['sudo'] if is_sudo else []
+        _cmd.extend([f'{self.path}{self.name}', *_parse_command(command), *_args])
+
+        self._log_request(command, _args, is_sudo)
         start_time = time.time()
-
-        _cmd = []
-
-        if self.sudo:
-            _cmd.append("sudo")
-
-        _cmd.extend([
-            f'{self.path}{self.name}', *command.split(' '),
-            *[arg for arg_group in _args for arg in arg_group.split(' ')]
-        ])
 
         process = Popen(_cmd, stdout=PIPE, stderr=PIPE)
         output, error = (_bytes.decode().strip() for _bytes in process.communicate())
@@ -51,3 +47,24 @@ class BaseCLI:
         self._log_response(process, output, error, start_time, end_time)
 
         return CLIResponse(status=ReturnCodes(process.returncode), output=output, error=error)
+
+
+def _parse_command(command):
+    commands = []
+    chars = []
+    is_quoted = False
+
+    if not command:
+        return commands
+
+    for char in command:
+        if char == '"':
+            is_quoted = not is_quoted
+        if char in [' ', '='] and not is_quoted:
+            commands.append(''.join(chars))
+            chars = []
+        else:
+            chars.append(char)
+    commands.append(''.join(chars))
+
+    return commands
